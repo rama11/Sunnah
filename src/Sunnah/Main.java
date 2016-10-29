c481f6f74b2fff40700965e96074d2c070db6737
Rama <prof.agastyo@gmail.com>
2016-10-29 23:07:10 +0700

    * Mboh nambai opo iki

--- src/Sunnah/Main.java
+++ src/Sunnah/Main.java
@@ -6,6 +6,7 @@
 package Sunnah;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -27,8 +28,16 @@ public class Main {
         Player user = new Player();
         user.setFrist(nama, pass);
         
-        // Menampilkan object yang baru di buat
-        user.showPlayer();
+        // Membuat object untuk Classing player
+        Class userClassed = new Class(user.getName(),user.getPass());
+        userClassed.setPlayerClass();
+        
+        // Menampilkan hasil object Final
+        userClassed.showPlayer();
+        
+        //System.out.println("\n Mengerluarkan sekali lagi \n");
+        //System.out.println("Nama2 player : " + user.getName());
+        //System.out.println("Pass2 player : " + user.getPass());
         
     }
 }
